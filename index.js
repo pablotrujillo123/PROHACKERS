187324bddc72dc35bb810f40ed9ad81a10d50f81
pablotrujillo123 <89481044+pablotrujillo123@users.noreply.github.com>
2023-02-09 11:31:23 -0600

    cmabios

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas')
+    const c = canvas.getcontext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+    c.fillstyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
