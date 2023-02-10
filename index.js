fc5b1fa961f04f98a2ad1eb924fa1557e0589907
maubaqueiro <mauricio.bmiranda@prepafrayangelico.edu.mx>
2023-02-09 19:25:39 -0600

    hola

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-const canvas = document.querySelector('canvas')
-    const c = canvas.getcontext('2d')
+const canvas = document.querySelector ('canvas')
+const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
 
-    c.fillstyle = 'white'
-    c.fillRect(0, 0, canvas.width, canvas.height)
+class Player {
+    constructor(position){
+        this.position = position
+        this.velocity = {
+            x: 0,
+            y: 1,
+        }
+    }
+
+    draw () {
+      c.fillStyle = red 
+      c.fillRect (this.position.x, this.position.y, 100, 100)
+    }
+
+    update() {
+        this.draw ()
+        this.position.y += this.velocity.y
+        this.velocity.y += 0.5
+    }
+}
+
+const player = new Player ({
+    x: 0,
+    y: 0,
+})
+const player2 = new Player ({
+    x: 300,
+    y: 100,
+})
+
+function animate ( ){
+    window,requestAnimationFrame (animate)
+c.fillStyle = 'white'
+c.fillRect (0,0, canvas.width, canvas.height)
+player.update ()
+player2.update ()
+}
+
