acae97de1f326e112f794a9725c80de03ba7ecb0
jorgega23 <jorge.garregoitia@prepafrayangelico.edu.mx>
2023-05-02 19:55:01 -0600

    16
    .

--- JUEGO.js
+++ JUEGO.js
@@ -10,8 +10,8 @@ const scaledCanvas = {
 }
 
 const floorCollisions2D = []
-for (let i = 0; i < floorCollisions.length; i += 36) {
-  floorCollisions2D.push(floorCollisions.slice(i, i + 36))
+for (let i = 0; i < floorCollisions2D.length; i += 36) {
+  floorCollisions2D.push(floorCollisions2D.slice(i, i + 36))
 }
 
 const collisionBlocks = []
@@ -19,7 +19,7 @@ floorCollisions2D.forEach((row, y) => {
   row.forEach((symbol, x) => {
     if (symbol === 202) {
       collisionBlocks.push(
-        new CollisionBlock({
+        new collisionBlocks({
           position: {
             x: x * 16,
             y: y * 16,
@@ -31,8 +31,8 @@ floorCollisions2D.forEach((row, y) => {
 })
 
 const platformCollisions2D = []
-for (let i = 0; i < platformCollisions.length; i += 36) {
-  platformCollisions2D.push(platformCollisions.slice(i, i + 36))
+for (let i = 0; i < platformCollisions2D.length; i += 36) {
+  platformCollisions2D.push(platformCollisions2D.slice(i, i + 36))
 }
 
 const platformCollisionBlocks = []
@@ -40,7 +40,7 @@ platformCollisions2D.forEach((row, y) => {
   row.forEach((symbol, x) => {
     if (symbol === 202) {
       platformCollisionBlocks.push(
-        new CollisionBlock({
+        new collisionBlocks({
           position: {
             x: x * 16,
             y: y * 16,
@@ -61,46 +61,46 @@ const player = new Player({
   },
   collisionBlocks,
   platformCollisionBlocks,
-  imageSrc: './img/warrior/Idle.png',
+  imageSrc: 'img/jugador/Idle.png',
   frameRate: 8,
   animations: {
     Idle: {
-      imageSrc: './img/warrior/Idle.png',
+      imageSrc: 'img/jugador/Idle.png',
       frameRate: 8,
       frameBuffer: 3,
     },
     Run: {
-      imageSrc: './img/warrior/Run.png',
+      imageSrc: 'img/jugador/Run.png',
       frameRate: 8,
       frameBuffer: 5,
     },
     Jump: {
-      imageSrc: './img/warrior/Jump.png',
+      imageSrc: 'img/jugador/Jump.png',
       frameRate: 2,
       frameBuffer: 3,
     },
     Fall: {
-      imageSrc: './img/warrior/Fall.png',
+      imageSrc: 'img/jugador/Fall.png',
       frameRate: 2,
       frameBuffer: 3,
     },
     FallLeft: {
-      imageSrc: './img/warrior/FallLeft.png',
+      imageSrc: 'img/jugador/FallLeft.png',
       frameRate: 2,
       frameBuffer: 3,
     },
     RunLeft: {
-      imageSrc: './img/warrior/RunLeft.png',
+      imageSrc: 'img/jugador/RunLeft.png',
       frameRate: 8,
       frameBuffer: 5,
     },
     IdleLeft: {
-      imageSrc: './img/warrior/IdleLeft.png',
+      imageSrc: 'img/jugador/IdleLeft.png',
       frameRate: 8,
       frameBuffer: 3,
     },
     JumpLeft: {
-      imageSrc: './img/warrior/JumpLeft.png',
+      imageSrc: 'img/jugador/JumpLeft.png',
       frameRate: 2,
       frameBuffer: 3,
     },
@@ -121,7 +121,7 @@ const background = new Sprite({
     x: 0,
     y: 0,
   },
-  imageSrc: './img/background.png',
+  imageSrc: 'img/background.png',
 })
 
 const backgroundImageHeight = 432
@@ -208,3 +208,4 @@ window.addEventListener('keyup', (event) => {
       break
   }
 })
+     
